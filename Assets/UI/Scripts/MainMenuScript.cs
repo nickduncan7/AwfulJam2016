d7d465a245dbc495b83669e378a8bbfe26c752c0
Nick Duncan <sorrowspear@gmail.com>
2016-07-31 12:05:22 -0600

    main menu buttons

---
+++ Assets/UI/Scripts/MainMenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void LoadLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TestScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
